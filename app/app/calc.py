1f4442d6d5cc7063b55be0af319ef26db692bc86
root <khajavi.peyman@gmail.com>
2025-09-07 14:58:39 +0330

    feat: add user and usermanagement

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,5 +2,5 @@ def add(a: int, b: int) -> int:
     return a + b
 
 
-def subtract(a: int, b: int) -> int:
+def substract(a: int, b: int) -> int:
     return a - b
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -13,7 +13,7 @@ class CommandTests(SimpleTestCase):
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
-    def test_wait_for_db_delay(self, patched_check, _):
+    def test_wait_for_db_delay(self, _, patched_check):
         patched_check.side_effect = [psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
         call_command('wait_for_db')
